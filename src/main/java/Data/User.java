d97000cdc45e03895d3cfb4eb82844b1948abd95
Sinan Jasar <sinanjasar@Sinans-MacBook-Pro.local>
2019-02-27 14:31:57 +0100

    added data mapper with getUser(String username) and insertUser(String username, String password, float balance) methods

--- src/main/java/Data/User.java
+++ src/main/java/Data/User.java
@@ -10,5 +10,26 @@ package Data;
  * @author sinanjasar
  */
 public class User {
+    private String username;
+    private String password;
+    private float balance;
+
+    public User(String username, String password, float balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
     
 }
