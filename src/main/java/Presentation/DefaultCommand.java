811be668d0b96aedea5db332a1634e2554dd3541
Sinan Jasar <sinanjasar@Sinans-MacBook-Pro.local>
2019-02-28 15:09:26 +0100

    Fixed some errors

---
+++ src/main/java/Presentation/DefaultCommand.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author sinanjasar
+ */
+public class DefaultCommand extends Command {
+
+    public DefaultCommand() {
+    }
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+                    /* TODO output your page here. You may use following sample code. */
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>Default page</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1> Default page </h1>");
+                    out.println("</body>");
+                    out.println("</html>");
+                }
+    }
+    
+}
