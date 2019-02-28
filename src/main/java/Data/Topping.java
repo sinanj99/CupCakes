246a1e0517fb78526f26850a7044e9a716475841
Younes2630 <t.piskorczyk@yahoo.dk>
2019-02-28 14:47:01 +0100

    klasser og DAO

---
+++ src/main/java/Data/Topping.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author Younes
+ */
+public class Topping {
+    
+    private float price;
+    private String flavour;
+
+    public Topping(float price, String flavour) {
+        this.price = price;
+        this.flavour = flavour;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getFlavour() {
+        return flavour;
+    }
+    
+    
+}
